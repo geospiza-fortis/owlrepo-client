6fdeba2b127019e3f6b7172c2a42b28ceafd12ff
geospiza <geospiza@protonmail.com>
2021-02-18 20:26:26 -0800

    Update guide to include etc and mastery books

--- src/routes/Guide.svelte
+++ src/routes/Guide.svelte
@@ -38,19 +38,84 @@
       10: `rgba(163, 195, 176, ${opacity})`,
       30: `rgba(234, 210, 184, ${opacity})`,
       60: `rgba(157, 151, 188, ${opacity})`,
-      70: `rgba(222, 167, 161, ${opacity})`
+      70: `rgba(222, 167, 161, ${opacity})`,
+      etc: `rgba(174, 188, 110, ${opacity})`,
+      mastery: `rgba(103, 154, 125, ${opacity})`
     }[percent];
   }
 
   onMount(async () => {
+    // TODO: I wish I had some documentation on the schema of these...
     let index = await getData("/api/v1/query/search_item_index");
     let category = await getData("/api/v1/query/mllib_scrolls_category");
 
+    console.log(category);
     // only keep scrolls that are categoriezed and exist in the database
     category_data = {};
     for (let i = 0; i < category.length; i++) {
       category_data[category[i].name] = category[i];
     }
+
+    // also include some other things that are not necessarily scrolls
+    let masterybooks = index.filter(row =>
+      row.search_item.includes("Mastery Book")
+    );
+    for (let item of masterybooks) {
+      category_data[item.search_item] = {
+        percent: "mastery",
+        category: item.search_item
+          .split("]")[1]
+          .trim()
+          .toLowerCase(),
+        // could be better...
+        stat: item.search_item.includes("20") ? "20" : "30"
+      };
+    }
+
+    category_data = {
+      ...category_data,
+      "Clean Slate Scroll 20%": {
+        percent: "etc",
+        category: "css",
+        stat: "20%"
+      },
+      "Clean Slate Scroll 1%": {
+        percent: "etc",
+        category: "css",
+        stat: "1%"
+      },
+      "Chaos Scroll 60%": {
+        percent: "etc",
+        category: "chaos scroll",
+        stat: ""
+      },
+      "White Scroll": {
+        percent: "etc",
+        category: "white scroll",
+        stat: ""
+      },
+      "Onyx Apple": {
+        percent: "etc",
+        category: "onyx apple",
+        stat: ""
+      },
+      "Zombie's Lost Gold Tooth": {
+        percent: "etc",
+        category: "gold tooth",
+        stat: ""
+      },
+      "Dragon Scale": {
+        percent: "etc",
+        category: "dragon scale",
+        stat: ""
+      },
+      "Piece of Time": {
+        percent: "etc",
+        category: "piece of time",
+        stat: ""
+      }
+    };
+
     price_data = [];
     // we need a method of matching items
     for (let i = 0; i < index.length; i++) {
@@ -79,15 +144,19 @@
   <div class="container full-width guide">
     <div class="card-columns">
       {#each Object.keys(price_data).sort() as key}
-        {#each chunkList(sortBy(price_data[key].filter(x => x.p25 > 350000), [
-            'category',
-            'stat'
-          ]), 15) as chunk, i}
+        {#each chunkList(sortBy(
+            price_data[key].filter(x => x.p25 > 350000 || x.percent == 'etc'),
+            ['category', 'stat']
+          ), 15) as chunk, i}
           <div
             class="card"
             style="background-color: {getBackgroundColor(key)};">
             <div class="card-header">
-              <h5>{key}% Scrolls</h5>
+              {#if parseInt(key)}
+                <h5>{key}% Scrolls</h5>
+              {:else}
+                <h5>{{ mastery: 'Mastery Book', etc: 'Etcetera' }[key]}</h5>
+              {/if}
             </div>
 
             <div class="card-body">
