6d1bd966f9079c631c29a4226f3eca189c6c4ef3
geospiza <geospiza@protonmail.com>
2021-02-18 23:47:10 -0800

    Fix listing page by turning it into a slug

--- src/routes/listing/[slug].svelte
+++ src/routes/listing/[slug].svelte
@@ -3,9 +3,12 @@
   import SummaryView from "../../components/SummaryView.svelte";
   import { Stretch } from "svelte-loading-spinners/src";
   import Tabulator from "tabulator-tables";
-  export let params;
 
-  let uid = params.listing_id;
+  import { stores } from "@sapper/app";
+  const { page } = stores();
+  const { slug } = $page.params;
+
+  let uid = slug;
   let url = `/api/v1/data/${uid}/slim.json`;
 
   let flattened = null;
