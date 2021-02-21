2d882131f0386f07e5ac2f01a0be4f3df7cd66a4
geospiza <geospiza@protonmail.com>
2021-02-20 20:28:39 -0800

    Add redirect of merchants to home page

---
+++ src/routes/merchants.svelte
@@ -0,0 +1,6 @@
+<script context="module">
+  export async function preload(page, session) {
+    // This route no longer exists, redirect to the home page
+    return this.redirect(301, "/");
+  }
+</script>
