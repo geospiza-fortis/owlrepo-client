ace30ed067529c929909e29e186ab497a35a3048
geospiza <geospiza@protonmail.com>
2021-03-03 21:50:07 -0800

    Use proxy to cloudrun url for v1 api

--- src/server.js
+++ src/server.js
@@ -1,12 +1,23 @@
 import sirv from "sirv";
 import polka from "polka";
 import compression from "compression";
+import { createProxyMiddleware } from "http-proxy-middleware";
 import * as sapper from "@sapper/server";
 
-const { PORT, NODE_ENV } = process.env;
+const { PORT, NODE_ENV, OWLREPO_URL } = process.env;
 const dev = NODE_ENV === "development";
 
-polka() // You can also use Express
+const server = polka();
+
+server.use(
+  "/api/v1",
+  createProxyMiddleware({
+    target: OWLREPO_URL,
+    changeOrigin: true,
+  })
+);
+
+server
   .use(
     compression({ threshold: 0 }),
     sirv("static", { dev }),
