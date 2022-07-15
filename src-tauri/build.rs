4f2dcbee7d3e7306f3ed5005eef7f403b8bb1a58
geospiza <geospiza@protonmail.com>
2022-07-15 01:20:08 -0700

    Download node binary and try to run as a sidecar

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,39 @@
+use std::fs::File;
+use std::io::Read;
+use std::io::Write;
+
+const VERSION: &str = "v16.16.0";
+
+// https://stackoverflow.com/questions/50471218/how-to-unzip-a-reqwest-hyper-response-using-streams
+// https://stackoverflow.com/questions/31192956/whats-the-de-facto-way-of-reading-and-writing-files-in-rust-1-x
+fn download_node_zip() {
+    if std::path::Path::new("target/node.exe").exists() {
+        return;
+    }
+    let mut tmpfile = tempfile::tempfile().unwrap();
+    println!("{:#?}", tmpfile);
+    reqwest::blocking::get(format!(
+        "https://nodejs.org/dist/{VERSION}/node-{VERSION}-win-x64.zip"
+    ))
+    .unwrap()
+    .copy_to(&mut tmpfile)
+    .unwrap();
+    let mut zip = zip::ZipArchive::new(tmpfile).unwrap();
+    // write the node file to the target directory
+    let mut file = match zip.by_name(format!("node-{VERSION}-win-x64/node.exe").as_str()) {
+        Ok(file) => file,
+        Err(..) => panic!("node not found in zip"),
+    };
+    let mut contents = Vec::new();
+    file.read_to_end(&mut contents).unwrap();
+    let mut output_file = File::create("target/node.exe").unwrap();
+    output_file.write_all(&contents).unwrap();
+    // copy file with version
+    let mut output_file = File::create("target/node.exe-x86_64-pc-windows-msvc.exe").unwrap();
+    output_file.write_all(&contents).unwrap();
+}
+
 fn main() {
-    tauri_build::build()
+    download_node_zip();
+    tauri_build::build();
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,34 @@
     windows_subsystem = "windows"
 )]
 
+use tauri::{
+    api::process::{Command, CommandEvent},
+    Manager,
+};
+
+// https://github.com/tauri-apps/tauri/blob/dev/examples/sidecar/package.json
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            let window = app.get_window("main").unwrap();
+            tauri::async_runtime::spawn(async move {
+                let (mut rx, mut child) = Command::new_sidecar("target/node.exe")
+                    .expect("failed to setup `app` sidecar")
+                    .spawn()
+                    .expect("Failed to spawn packaged node");
+
+                while let Some(event) = rx.recv().await {
+                    if let CommandEvent::Stdout(line) = event {
+                        window
+                            .emit("message", Some(format!("'{}'", line)))
+                            .expect("failed to emit event");
+                        child.write("message from Rust\n".as_bytes()).unwrap();
+                    }
+                }
+            });
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
