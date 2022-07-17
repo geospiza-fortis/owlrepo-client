2a72c1c98665eae4a05efed1dc73e18a2abd1ef3
geospiza <geospiza@protonmail.com>
2022-07-17 11:47:17 -0700

    Update tauri app to point to prod and to use same version number

--- src/routes/api/v2/status.js
+++ src/routes/api/v2/status.js
@@ -1,5 +1,4 @@
 export async function GET({ params }) {
-  const { prefix } = params;
   return {
     body: {
       status: "ok",
@@ -8,10 +7,12 @@ export async function GET({ params }) {
       version: "__VERSION__",
       git_sha: "__GIT_COMMIT__",
       build_time: "__BUILD_TIME__",
-      tauri: import.meta.env.VITE_TAURI == "true",
       tauri_version: "__TAURI_VERSION__",
       ...(import.meta.env.MODE == "development"
-        ? { owlrepo_api_url: import.meta.env.VITE_OWLREPO_URL }
+        ? {
+            tauri: import.meta.env.VITE_TAURI == "true",
+            owlrepo_api_url: import.meta.env.VITE_OWLREPO_URL,
+          }
         : {}),
     },
     status: 200,
